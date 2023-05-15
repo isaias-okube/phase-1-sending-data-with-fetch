6042e03ba1efd262054110e2982cedf15e91f69b
ISAIAS <issuyep@yahoo.com>
2023-05-14 18:14:43 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users" , configurationObject)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        const newId = object.id;
+        document.body.innerHTML += newId;
+      })
+      .catch(function(error) {
+        document.body.innerHTML += error.message;
+      });
+  }
+  
